ea6d1bf4f23f823813a687e291e6703dcbdc58e6
Gabriel Diegues Figueiredo Rocha <127216244+gabrieldfr@users.noreply.github.com>
2023-11-12 22:27:27 -0300

    Create LeituraDosDadosServidor.py

---
+++ LeituraDosDadosServidor.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as mqtt
+mqtt_topics = [("/TEF/lamp108/attrs/luminosity", 0), ("/TEF/lamp108/attrs/led", 0)]
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    # client.subscribe("/TEF/lamp108/attrs/l")
+    client.subscribe(mqtt_topics)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("46.17.108.113", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
